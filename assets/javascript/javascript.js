f7ff4184366eb74b3f464d14c8c6ba3ce925251a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-17 00:22:13 +0000

    Added button

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'black';
+context.fillRect(10, 10, 100, 100);
+
+let btn = document.getElementById('roll-button');
+
