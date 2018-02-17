646928aa5173e30018b0eeecbb421b889d3cda3c
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-17 01:52:26 +0000

    Updated to completion

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,43 @@
+// Global Variables
+
+// Set canvas size
+let canvasHeight = 107;
+let canvasWidth = 107;
+
+// The position on the spritesheet of each sprite.
+// spriteY remains constant
+// Face 1 = 0, Face 2 = -107, Face 3 = -214 etc
+let spriteX = 0; 
+const spriteY = 0;
+
+// The width of each frame on the spritesheet
+let spriteWidth = -107;
+
+// Create the dice instance
+let dice = new Image();
+dice.src = 'DiceSprite.png';
+
+// Create the canvas
 let canvas = document.getElementById('canvas');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 let context = canvas.getContext('2d');
 
-context.fillStyle = 'black';
-context.fillRect(10, 10, 100, 100);
-
-let btn = document.getElementById('roll-button');
+function updateFrame(num){
+    // The logic for moving backwards along the spritesheet
+    spriteX = (num * spriteWidth) - spriteWidth;
+}
 
 function randomNumber(num){
     let random = Math.floor(Math.random()*((num + 1) - 1) + 1);
+    // console.log for testing purposes
+    console.log(random);
     return random;
 }
 
 function buttonClick(){
-    let output = document.getElementById('roll-number');
-    output.innerHTML = randomNumber(6);
+    let output = randomNumber(6);
+    updateFrame(output);
+    // Draw on the canvas
+    context.drawImage(dice, spriteX, spriteY);
 }
